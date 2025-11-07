a45f1a3f91ba9603c73a041eaebcb758e3628515
sophie nguyen <sophie.nguyen@berkeley.edu>
2025-11-06 18:04:42 -0800

    profile task

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,10 +2,21 @@ import "../global.css"
 import { Text, View } from "@/components/Themed";
 import Button from "@/components/Button";
  
+// TODO: build the profile screen! this should use the lists of provided preferences and dynamically render
+// buttons for each preference underneath its corresponding section (meaning don't hardcode each string!)
+// you might find .map() helpful. 
 export default function ProfileScreen() {
+  const dietary = ["No Preference", "Vegetarian", "Vegan", "Halal", "Kosher"];
+  const cuisines = ["Italian", "Mexican", "Japanese", "Indian", "Thai", "American"];
+
   return (
     <View className="flex-1 items-center justify-center">
-      <Button label = "this label has been hardcoded"></Button>
+      <Text className="text-6xl font-bold mb-4">Your Profile</Text>
+      <Button label = "hardcoded text"></Button>
+      <Text className="text-6xl font-bold mb-4">Dietary Restrictions</Text>
+      <Button label = "hardcoded text"></Button>
+      <Text className="text-6xl font-bold mb-4">Cusines</Text>
+      <Button label = "hardcoded text"></Button>
     </View>
   );
 }
